941c5a26812672e1619c01b9ff243929eeea05d5
Mars Hou <houjianchao1204@gmail.com>
2018-05-19 19:44:30 +0800

    modify print method

--- main/rating_main.py
+++ main/rating_main.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from main import data_download, data_save, data_send, data_parse
+from main import data_download, data_save, data_send, data_parse, show_process
 from main.sortby import SortBy
 
 URL = r'https://itunes.apple.com/rss/customerreviews/page=%d/id=%d/sortby=%s/json?l=en&&cc=cn'
@@ -11,6 +11,7 @@ APP_ID = 1084660392
 class RatingMain(object):
 
     def __init__(self):
+        self.show_process = show_process.ShowProcess(10)
         self.download = data_download.DataDownLoad()
         self.parse = data_parse.DataParse()
         self.save = data_save.DataSave()
@@ -21,14 +22,16 @@ class RatingMain(object):
         # rating_data = self.download.download_html_data(rating_url)
         # print(rating_data)
         data = self.__start(1)
+        self.show_process.close()
         file = self.save.save_data(data)
         # self.send.send_email(file)
         # print(data)
 
     def __start(self, page):
         url = URL % (page, APP_ID, SortBy.MOST_HELPFUL)
-        print('start download page %d' % page)
+        # print('\rProcess %d0%%' % page, end='', flush=True)
         json_data = self.download.download_data(url)
+        self.show_process.show_process(page)
         last = self.parse.get_last_page(json_data)
         array_data = self.parse.get_data(json_data)
         page += 1
